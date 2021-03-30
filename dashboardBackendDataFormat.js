6875ba9de1540104848066f844af52d7649925d9
maxbaiyu <73329657+maxbaiyu@users.noreply.github.com>
2021-03-30 11:43:24 +0800

    Add files via upload

---
+++ dashboardBackendDataFormat.js
@@ -0,0 +1,311 @@
+// pie chart
+
+[
+    { "value": 586, "name": "New" },
+    { "value": 57, "name": "New to Replace" },
+    { "value": 33, "name": "Reuse with Version Change" },
+    { "value": 40, "name": "Reuse with Minor Change" },
+    { "value": 176, "name": "Reuse Direct" }
+]
+
+// Order Delivery Duration  1 或者 2 都可以
+
+1.[15, 58, 13, 5, 4, 2, 1]
+
+2.[
+    { "count": 15, "name": "1 month" },
+    { "count": 58, "name": "2 months" },
+    { "count": 13, "name": "3 months" },
+    { "count": 5, "name": "4 months" },
+    { "count": 4, "name": "5 months" },
+    { "count": 2, "name": "6 months" },
+    { "count": 1, "name": "6 months+" }
+]
+
+
+// order tracking 
+[
+    {
+        name: 'Order Received',
+
+        data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    },
+    {
+        name: 'Delivered for Testing',
+        data: []
+    },
+    {
+        name: 'Delivered for Production',
+        data: []
+    }
+]
+
+
+// Top10 
+// 1 所有数据一把返回
+[
+    {
+        "count": 11,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "Term Deposit Transaction History Enquiry"
+    },
+    {
+        "count": 6,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "individual customer idenfication modification"
+    },
+    {
+        "count": 5,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "Individual customer demographics Modification"
+    },
+    {
+        "count": 3,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "individual customer demographic enquiry"
+    },
+    {
+        "count": 2,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "Loan Account Transaction History Enquiry"
+    },
+    {
+        "count": 1,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "Individual customer identification enquiry"
+    },
+    {
+        "count": 1,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "TD Account Information Enquiry"
+    },
+    {
+        "count": 1,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "TD account information enquiry"
+    },
+    {
+        "count": 1,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "accounts demand deposit account enquiry"
+    },
+    {
+        "count": 1,
+        "groupBy": "apiName",
+        "demandClassfication": "03",
+        "name": "customer address modification"
+    },
+
+    {
+        "count": 13,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "Term Deposit Account Opening"
+    },
+    {
+        "count": 10,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "transactions-domestic-txns-cmb-ci-gb-hbeu"
+    },
+    {
+        "count": 7,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "transactions-domestic-txns-dbb-gb-hbeu"
+    },
+    {
+        "count": 5,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "accounts-account-lists-cmb-obci-gb-hbeu"
+    },
+    {
+        "count": 5,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "business-internet-bankings-cmb-ci-gb-hbeu"
+    },
+    {
+        "count": 4,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "accounts-dc-authorisations--rps-gb-hbeu"
+    },
+    {
+        "count": 3,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "accounts-account-transactions--rps-gb-hbeu"
+    },
+    {
+        "count": 2,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "customer limit modification"
+    },
+    {
+        "count": 1,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "DD account information modification"
+    },
+    {
+        "count": 1,
+        "groupBy": "apiName",
+        "demandClassfication": "04",
+        "name": "accounts demand deposit account enquiry"
+    },
+
+    {
+        "count": 31,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Account List"
+    },
+    {
+        "count": 18,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Individual Customer Identification Enquiry"
+    },
+    {
+        "count": 12,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "customer address enquiry"
+    },
+    {
+        "count": 9,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Employee Banking Information Enquiry"
+    },
+    {
+        "count": 8,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Target Employee Banking Information Enquiry"
+    },
+    {
+        "count": 6,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Employee Banking Information Modification"
+    },
+    {
+        "count": 5,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Customer Target Employer List Enquiry"
+    },
+    {
+        "count": 5,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Employee Banking Information Modification"
+    },
+    {
+        "count": 3,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Employee Banking Information Enquiry"
+    },
+    {
+        "count": 2,
+        "groupBy": "apiName",
+        "demandClassfication": "05",
+        "name": "Individual Customer Identification Enquiry"
+    },
+
+    {
+        "count": 123,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Core Banking Control Data"
+    },
+    {
+        "count": 78,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Core Banking Product Design"
+    },
+    {
+        "count": 52,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Pricing"
+    },
+    {
+        "count": 21,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Cross Product Rules"
+    },
+    {
+        "count": 19,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Interest Plans"
+    },
+    {
+        "count": 17,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Campaign and Prospect Management"
+    },
+    {
+        "count": 16,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Customer Onboarding & Maintenance"
+    },
+    {
+        "count": 12,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Customer Data - Downstream systems"
+    },
+    {
+        "count": 8,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "KYC"
+    },
+    {
+        "count": 7,
+        "groupBy": "feature",
+        "demandClassfication": "03",
+        "name": "Customer Limits"
+    }
+]
+
+
+//2 按照下拉框返回
+
+{
+    "top10Name": [
+        "Account List",
+        "Individual Customer Identification Enquiry",
+        "Term Deposit Account Opening",
+        "customer address enquiry",
+        "Employee Banking Information Enquiry",
+        "Term Deposit Transaction History Enquiry",
+        "Employee Banking Information Modification",
+        "transactions-domestic-txns-cmb-ci-gb-hbeu",
+        "Target Employee Banking Information Enquiry",
+        "transactions-domestic-txns-dbb-gb-hbeu"
+    ],
+        "top10Data": [7, 8, 10, 11, 11, 12, 12, 13, 20, 31]
+}
+
+
